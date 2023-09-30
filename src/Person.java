2326bcb1aa6fb1dd6c3a4e5dd936a9e00e10fb7a
Skillnhard <sashastronk1@gmail.com>
2023-09-30 13:40:46 +0600

    Add class Person

---
+++ src/Person.java
@@ -0,0 +1,12 @@
+public class Person {
+    String name;
+    int age;
+
+    public void eat() {
+        System.out.println("Eating");
+    }
+
+    public void test() {
+        System.out.println("Hi!");
+    }
+}
